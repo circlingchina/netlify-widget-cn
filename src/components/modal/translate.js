95bbcc77147f4265752c3ec034be9f2d48c4d9ef
yiliangt5 <1404615+yiliangt5@users.noreply.github.com>
2020-10-12 12:02:12 +0200

    Update translations

--- src/components/modal/translate.js
+++ src/components/modal/translate.js
@@ -1,7 +1,8 @@
 const TRANS_MAPPING = {
   "A user with this email address has already been registered": "该邮箱已经注册",
   "Email not confirmed": "账号不存在或密码错误",
-  "No user found with this email": "账号不存在或密码错误"
+  "No user found with this email": "账号不存在或密码错误",
+  "Password reset request for migrated users": "用户系统已升级，请通过“忘记密码”来重置密码并登陆"
 };
 
 export default function translate(error_description) {
